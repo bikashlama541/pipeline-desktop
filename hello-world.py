2eca04d96399a0673508723873951a4f51175fa8
root <root@bikash.localdomain>
2019-10-15 02:54:35 -0500

    tried new changes

--- hello-world.py
+++ hello-world.py
@@ -1,5 +1,5 @@
 
-name = raw_input("Enter your name: ")
+name = input("Enter your name: ")
 age = input("Enter your age: ")
 
 print ("Hello your name is " + name + " and your age is " + str(age) + " years old") 
